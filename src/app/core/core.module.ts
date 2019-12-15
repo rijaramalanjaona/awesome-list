971825ca994719078c6042a7da27ee23f97dd017
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2019-12-15 12:56:23 +0100

    generation des modules core shared public protected

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PublicModule } from '../public/public.module';
+import { ProtectedModule } from '../protected/protected.module';
+
+
+
+@NgModule({
+	declarations: [],
+	imports: [
+		CommonModule,
+		PublicModule,
+		ProtectedModule
+	]
+})
+export class CoreModule { }
