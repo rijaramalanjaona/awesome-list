7f5e760c07dd4e919065a39ef6e4731323fe8dff
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2019-12-15 14:23:47 +0100

    generation modules de fonctionnalites

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -2,13 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ProtectedRoutingModule } from './protected-routing.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { ParametersModule } from './parameters/parameters.module';
+import { PlanningModule } from './planning/planning.module';
+import { ProfilModule } from './profil/profil.module';
+import { WorkdayModule } from './workday/workday.module';
 
 
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule,
-		ProtectedRoutingModule
+		ProtectedRoutingModule,
+		DashboardModule,
+		ParametersModule,
+		PlanningModule,
+		ProfilModule,
+		WorkdayModule
 	]
 })
 export class ProtectedModule { }
--- src/app/public/public.module.ts
+++ src/app/public/public.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PublicRoutingModule } from './public-routing.module';
+import { HomeModule } from './home/home.module';
+import { RegisterModule } from './register/register.module';
+import { LoginModule } from './login/login.module';
 
 
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule,
-		PublicRoutingModule
+		PublicRoutingModule,
+		HomeModule,
+		RegisterModule,
+		LoginModule
 	]
 })
 export class PublicModule { }
