8dc7dde563640200323e38e3a885b977fb0f221e
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2019-12-15 13:04:16 +0100

    s'assurer que le CoreModule n'est importe qu'une seule fois

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {NgModule, Optional, SkipSelf} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PublicModule } from '../public/public.module';
 import { ProtectedModule } from '../protected/protected.module';
@@ -13,4 +13,20 @@ import { ProtectedModule } from '../protected/protected.module';
 		ProtectedModule
 	]
 })
-export class CoreModule { }
+export class CoreModule {
+	/**
+	 * S’assurer que le CoreModule n’est importe qu’une seule fois
+	 *
+	 * @Optional: permet d’indiquer qu’une dependance est optionnelle.
+	 * Si la dependance n’est pas renseignee, alors c’est la valeur null qui est injectee
+	 *
+	 * @SkipSelf: indique au mecanisme d’injection de dependance que la resolution de cette dependance doit commencer a partir
+	 * de l’injecteur parent. Le CoreModule ne peut etre instancie qu’une fois, et depuis un module parent uniquement.
+	 *
+	 */
+	constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+		if (parentModule) {
+			throw new Error('CoreModule is already loaded');
+		}
+	}
+}
