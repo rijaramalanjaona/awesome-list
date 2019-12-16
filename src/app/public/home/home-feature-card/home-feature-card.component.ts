f9a5da519ff027d63a91041cd90ecfa2be4e6eb6
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2019-12-16 22:39:50 +0100

    mise en place de la page d'accueil

---
+++ src/app/public/home/home-feature-card/home-feature-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'al-home-feature-card',
+  templateUrl: './home-feature-card.component.html',
+  styles: []
+})
+export class HomeFeatureCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/public/home/home-features/home-features.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'al-home-features',
+  templateUrl: './home-features.component.html',
+  styles: []
+})
+export class HomeFeaturesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
