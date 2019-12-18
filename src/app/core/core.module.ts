22bdb499cde475f6920ee8307a132d89cf1e55d0
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2019-12-18 22:30:02 +0100

    ajout barre de navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,17 +3,19 @@ import { CommonModule } from '@angular/common';
 import { PublicModule } from '../public/public.module';
 import { ProtectedModule } from '../protected/protected.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
-	declarations: [],
+	declarations: [NavbarComponent],
 	imports: [
 		CommonModule,
 		PublicModule,
 		ProtectedModule,
 		BrowserAnimationsModule
-	]
+	],
+	exports: [NavbarComponent]
 })
 export class CoreModule {
 	/**
