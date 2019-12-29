de345fcf06a4bb197e4d32fee0473462b1402a23
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2019-12-29 22:43:08 +0100

    communication entre composants : intercepter les changements avec un setter

--- src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -6,6 +6,30 @@ import {Component, Input } from '@angular/core';
 	styles: []
 })
 export class PlanningWorkdayItemComponent {
-	@Input('workday') // pour le composant parent, la propriete recevant les donnees du composant est toujours workday.
-	currentWorkday; // utilisation d'un alias, a utiliser juste dans le composant lui-meme
+	private currentWorkday; // nom interne utilisé dans le composant
+
+	/**
+	 * le nom de la propriété d’entrée appelé par le composant parent sera le nom du setter, qui est workday.
+	 * Cette méthode prend en paramètre un workday.
+	 * Toutes les valeurs qui arriveront en entrée de ce composant seront envoyées en paramètre du setter,
+	 * et vous pouvez ensuite effectuer un traitement spécifique pour chacune de ses données.
+	 *
+	 */
+	@Input()
+	set workday(workday) {
+		this.currentWorkday = workday || {}; // if workday == null => currentWorkday = {}
+
+		// traitement spécifique d'affichage
+		if ('Lundi' === workday.dueDate) {
+			this.currentWorkday.dueDate += ' (Aujourd\'hui)';
+		}
+	}
+
+	/**
+	 * Pour masquer le nom de la propriété intermédiaire currentWorkday dans le template
+	 * Permet d'utiliser le même nom de propriété entre le composant parent et fils
+	 */
+	get workday() {
+		return this.currentWorkday;
+	}
 }
