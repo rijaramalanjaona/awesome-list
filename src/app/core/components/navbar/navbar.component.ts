0098f3d7643deec5b1350d0d7d99769127bd37be
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2019-12-30 16:23:19 +0100

    navigation dans le navbar component

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'al-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+	selector: 'al-navbar',
+	templateUrl: './navbar.component.html',
+	styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+	public homePath = 'home';
+	public loginPath = 'login';
+	public registerPath = 'register';
 
-  constructor() { }
+	constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
+	public isActive(page: string): boolean {
+		return this.router.isActive(page, true);
+	}
+
+	public navigate(page: string): void {
+		this.router.navigate([page]);
+	}
 }
