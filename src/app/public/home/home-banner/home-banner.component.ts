f80fd459407941eb4bb0e61cb7cb7caaa22ea3fa
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2019-12-30 17:49:40 +0100

    lien vers dashboard depuis la banniere

--- src/app/public/home/home-banner/home-banner.component.ts
+++ src/app/public/home/home-banner/home-banner.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'al-home-banner',
-  templateUrl: './home-banner.component.html',
-  styleUrls: ['./home-banner.component.scss']
+	selector: 'al-home-banner',
+	templateUrl: './home-banner.component.html',
+	styleUrls: ['./home-banner.component.scss']
 })
-export class HomeBannerComponent implements OnInit {
+export class HomeBannerComponent {
+	public prefix = 'app';
+	public dashboardPath = `${this.prefix}/dashboard`;
 
-  constructor() { }
+	constructor(private router: Router) { }
 
-  ngOnInit() {
-  }
-
+	public navigate(page: string): void {
+		this.router.navigate([page]);
+	}
 }
