ea3647ce2ce934d31808a69bd365231f6662a472
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-01-02 22:43:20 +0100

    formulaire inscription : mise en place

---
+++ src/app/public/register/register-form/register-form.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
+
+@Component({
+	selector: 'al-register-form',
+	templateUrl: './register-form.component.html',
+	styles: []
+})
+export class RegisterFormComponent implements OnInit {
+
+	registerForm: FormGroup;
+
+	constructor(private fb: FormBuilder, private router: Router) { }
+
+	ngOnInit() {
+		this.registerForm = this.fb.group({
+			'name': '',
+			'email': '',
+			'password': ''
+		});
+	}
+
+	get name() {
+		return this.registerForm.get('name');
+	}
+
+	get email() {
+		return this.registerForm.get('email');
+	}
+
+	get password() {
+		return this.registerForm.get('password');
+	}
+
+	submit() {
+		// tslint:disable-next-line:no-console
+		console.info(this.name.value);
+		// tslint:disable-next-line:no-console
+		console.info(this.email.value);
+		// tslint:disable-next-line:no-console
+		console.info(this.password.value);
+		this.router.navigate(['/app/dashboard']);
+	}
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgxBootstrapModule } from './modules/ngx-bootstrap.module';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 
 
@@ -9,12 +10,14 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
 	declarations: [SidenavComponent],
 	imports: [
 		CommonModule,
-		NgxBootstrapModule
+		NgxBootstrapModule,
+		ReactiveFormsModule
 	],
 	exports: [
 		CommonModule,
 		NgxBootstrapModule,
-		SidenavComponent
+		SidenavComponent,
+		ReactiveFormsModule
 	]
 })
 export class SharedModule { }
