caa1a23498b56a574b5b0116c1288f4eae67994d
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-01-02 23:01:49 +0100

    formulaire inscription : validators

--- src/app/public/register/register-form/register-form.component.ts
+++ src/app/public/register/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 
 @Component({
@@ -15,9 +15,21 @@ export class RegisterFormComponent implements OnInit {
 
 	ngOnInit() {
 		this.registerForm = this.fb.group({
-			'name': '',
-			'email': '',
-			'password': ''
+			'name': ['', [
+				Validators.required,
+				Validators.minLength(4),
+				Validators.maxLength(20),
+				Validators.pattern('^[a-zA-Z0-9_-]*$')
+			]],
+			'email': ['', [
+				Validators.required,
+				Validators.email
+			]],
+			'password': ['', [
+				Validators.required,
+				Validators.minLength(6),
+				Validators.maxLength(20)
+			]]
 		});
 	}
 
