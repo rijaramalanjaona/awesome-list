531c68413433277c18e046d031b74e32fda9c962
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-01-02 23:56:37 +0100

    formulaire de connexion

--- src/app/public/login/login.module.ts
+++ src/app/public/login/login.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import {SharedModule} from '../../shared/shared.module';
 import { LoginComponent } from './login/login.component';
 import {LoginRoutingModule} from './login-routing.module';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 
 
 @NgModule({
-	declarations: [LoginComponent],
+	declarations: [LoginComponent, LoginFormComponent],
 	imports: [
 		SharedModule,
 		LoginRoutingModule // pour le lazy loading de LoginModule
