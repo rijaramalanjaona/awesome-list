9507fa9838b1fbffe137db65b08bf28da893ea3c
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-01-15 23:17:16 +0100

    Save user dans Firestore apres inscription

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,6 +3,8 @@ import {BehaviorSubject, Observable, of} from 'rxjs';
 import {User} from '../../shared/models/user';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
+import {switchMap} from 'rxjs/operators';
+import {UsersService} from './users.service';
 
 @Injectable({
 	providedIn: 'root'
@@ -14,7 +16,7 @@ export class AuthService {
 
 	public readonly user$: Observable<User|null> = this.user.asObservable();
 
-	constructor(private http: HttpClient) { }
+	constructor(private http: HttpClient, private usersService: UsersService) { }
 
 	public login(email: string, password: string): Observable<User|null> {
 		// TODO 1 faire un appel au backend
@@ -36,7 +38,18 @@ export class AuthService {
 			headers: new HttpHeaders({'Content-Type': 'application/json'})
 		};
 
-		return this.http.post<User>(url, data, httpOptions);
+		return this.http.post(url, data, httpOptions).pipe(
+			switchMap((responseData: any) => {
+				const jwt: string = responseData.idToken;
+				const user = new User({
+					email: responseData.email,
+					id: responseData.localId,
+					name
+				});
+
+				return this.usersService.save(user, jwt);
+			})
+		);
 	}
 
 	public logout(): Observable<null> {
---
+++ src/app/core/services/users.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {User} from '../../shared/models/user';
+import {Observable, of} from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {switchMap} from 'rxjs/operators';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class UsersService {
+
+	constructor(private http: HttpClient) { }
+
+	save(user: User, jwt: string): Observable<User|null> {
+		const url = `${environment.firebase.firestore.baseURL}/users?key=${environment.firebase.apiKey}`;
+
+		const data = this.getDataForFirestore(user);
+
+		const httpOptions = {
+			headers: new HttpHeaders({
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${jwt}`
+			})
+		};
+
+		return this.http.post(url, data, httpOptions).pipe(
+			switchMap((responseData: any) => {
+				return of(this.getUserFromFirestore(responseData.fields));
+			})
+		);
+	}
+
+	// mapping User to Firestore
+	private getDataForFirestore(user: User) {
+		return {
+			fields: {
+				id: { stringValue: user.id },
+				email: { stringValue: user.email },
+				name: { stringValue: user.name },
+				avatar: { stringValue: user.avatar },
+				pomodoroDuration: { integerValue: user.pomodoroDuration }
+			}
+		};
+	}
+
+	// mapping Firestore to User
+	private getUserFromFirestore(fields): User {
+		return new User({
+			id: fields.id.stringValue,
+			email: fields.email.stringValue,
+			name: fields.name.stringValue,
+			avatar: fields.avatar.stringValue,
+			pomodoroDuration: fields.pomodoroDuration.integerValue
+		});
+	}
+}
