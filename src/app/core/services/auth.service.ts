15530c9f800f96ff8d0fc8dbf05e56bfc8424abf
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-01-16 22:36:50 +0100

    Gestion des erreurs inscription

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,8 +3,9 @@ import {BehaviorSubject, Observable, of} from 'rxjs';
 import {User} from '../../shared/models/user';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
-import {switchMap, tap} from 'rxjs/operators';
+import {catchError, switchMap, tap} from 'rxjs/operators';
 import {UsersService} from './users.service';
+import {ErrorService} from './error.service';
 
 @Injectable({
 	providedIn: 'root'
@@ -16,7 +17,7 @@ export class AuthService {
 
 	public readonly user$: Observable<User|null> = this.user.asObservable();
 
-	constructor(private http: HttpClient, private usersService: UsersService) { }
+	constructor(private http: HttpClient, private usersService: UsersService, private errorService: ErrorService) { }
 
 	public login(email: string, password: string): Observable<User|null> {
 		// TODO 1 faire un appel au backend
@@ -51,7 +52,10 @@ export class AuthService {
 			}),
 
 			// maj état user
-			tap(user => this.user.next(user))
+			tap(user => this.user.next(user)),
+
+			// gestion erreurs
+			catchError(error => this.errorService.handleError(error))
 		);
 	}
 
