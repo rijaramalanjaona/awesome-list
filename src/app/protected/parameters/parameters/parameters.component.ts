ac4d1e5503d1eb17e21d1d51b473b0236d2fd675
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-01-28 22:59:41 +0100

    Parametres : formulaire

--- src/app/protected/parameters/parameters/parameters.component.ts
+++ src/app/protected/parameters/parameters/parameters.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'al-parameters',
-  templateUrl: './parameters.component.html',
-  styles: []
+	selector: 'al-parameters',
+	templateUrl: './parameters.component.html',
+	styles: []
 })
 export class ParametersComponent implements OnInit {
+	parametersForm: FormGroup;
+	pomodoros: number[] = [15, 20, 25, 30, 35, 40, 45, 50, 55, 60];
 
-  constructor() { }
+	constructor(private fb: FormBuilder) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.parametersForm = this.fb.group({
+			pomodoro: ['', [Validators.required]]
+		});
+	}
+
+	onSubmit() {
+		console.log(this.parametersForm.get('pomodoro').value);
+	}
 
 }
