3a617fc5d95513e277784b594a1538cca59acf4a
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-05-03 22:41:09 +0200

    workdaysService : ajout loader, toastr, gestion des erreurs

--- src/app/core/services/workdays.service.ts
+++ src/app/core/services/workdays.service.ts
@@ -3,13 +3,21 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Workday} from '../../shared/models/workday';
 import {environment} from '../../../environments/environment';
 import { Task } from 'src/app/shared/models/task';
+import {LoaderService} from './loader.service';
+import {ToastrService} from './toastr.service';
+import {ErrorService} from './error.service';
+import {catchError, finalize, tap} from 'rxjs/operators';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class WorkdaysService {
 
-	constructor(private http: HttpClient) { }
+	constructor(
+		private http: HttpClient,
+		private loaderService: LoaderService,
+		private toastrService: ToastrService,
+		private errorService: ErrorService) { }
 
 	save(workday: Workday) {
 		const url = `${environment.firebase.firestore.baseURL}/workdays?key=${environment.firebase.apiKey}`;
@@ -22,7 +30,22 @@ export class WorkdaysService {
 			})
 		};
 
-		return this.http.post(url, data, httpOptions);
+		// ajout du loader
+		this.loaderService.setLoading(true);
+
+		return this.http.post(url, data, httpOptions).pipe(
+			// ajout du toastr
+			tap(_ => this.toastrService.showToastr({
+				category: 'success',
+				message: 'Votre journée de travail a été enregistré avec succès.'
+			})),
+
+			// gestion des erreurs
+			catchError(error => this.errorService.handleError(error)),
+
+			// retire le loader
+			finalize(() => this.loaderService.setLoading(false))
+		);
 	}
 
 	private getWorkdayForFirestore(workday: Workday) {
