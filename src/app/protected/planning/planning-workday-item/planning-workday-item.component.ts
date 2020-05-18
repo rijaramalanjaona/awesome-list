e6c7cd873fb479e724d2b61bab9637de373fdab5
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-05-18 22:36:23 +0200

    intercepter le clic sur un workday depuis le planning

--- src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
+++ src/app/protected/planning/planning-workday-item/planning-workday-item.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Workday} from '../../../shared/models/workday';
+import {Router} from '@angular/router';
 
 @Component({
 	selector: 'al-planning-workday-item',
@@ -13,9 +14,26 @@ export class PlanningWorkdayItemComponent implements OnInit {
 	@Output()
 	workdayRemoved = new EventEmitter<string>();
 
+	constructor(private router: Router) {}
+
 	ngOnInit() {}
 
 	removeWorkday(displayDate: string) {
 		this.workdayRemoved.emit(displayDate);
+	}
+
+	/**
+	 * permet de faire une redirection vers la page d'édition de planning workday-form
+	 * url de la forme localhost:4200/app/workday?date=1587477909000
+	 */
+	goWorkday(workday: Workday) {
+		this.router.navigate(
+			['app/workday'],
+			{
+				queryParams: {
+					date: workday.dueDate
+				}
+			}
+		);
 	}
 }
