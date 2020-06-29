28d3a97f7079aabb2e2f7e5ba763cb369cb9959a
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-06-30 00:12:15 +0200

    fix bug remove workday (recall getWorkdayByUser)

--- src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
+++ src/app/protected/planning/planning-workday-list/planning-workday-list.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {Observable} from 'rxjs';
 import {Workday} from '../../../shared/models/workday';
 import {AuthService} from '../../../core/services/auth.service';
 import {WorkdaysService} from '../../../core/services/workdays.service';
-import {filter} from 'rxjs/operators';
 
 @Component({
 	selector: 'al-planning-workday-list',
@@ -12,22 +10,20 @@ import {filter} from 'rxjs/operators';
 })
 export class PlanningWorkdayListComponent implements OnInit {
 
-	workdays$: Observable<Workday>;
+	workdays: Workday[];
 
 	constructor(private authService: AuthService, private workdayService: WorkdaysService) { }
 
 	ngOnInit() {
 		const id: string = this.authService.currentUser.id;
-		this.workdays$ = this.workdayService.getWorkdayByUser(id);
+		this.workdayService.getWorkdayByUser(id).subscribe(
+			workdays => this.workdays = workdays
+		);
 	}
 
 	onWorkdayRemoved(workday: Workday) {
 		this.workdayService.remove(workday).subscribe(
-			_ => {
-				this.workdays$ = this.workdays$.pipe(
-					filter(currentWorkday => currentWorkday.id !== workday.id)
-				);
-			}
+			_ => this.workdays = this.workdays.filter(currentWorkday => currentWorkday.id !== workday.id)
 		);
 	}
 }
