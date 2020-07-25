832e158471247421bd295236c50446463f6dc4f5
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-07-25 12:02:09 +0200

    dashboard : ajouter la liste des taches

--- src/app/protected/dashboard/dashboard.module.ts
+++ src/app/protected/dashboard/dashboard.module.ts
@@ -3,11 +3,12 @@ import {SharedModule} from '../../shared/shared.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {DashboardRoutingModule} from './dashboard-routing.module';
 import { DashboardPomodoroProgressComponent } from './dashboard-pomodoro-progress/dashboard-pomodoro-progress.component';
+import { DashboardTaskItemComponent } from './dashboard-task-item/dashboard-task-item.component';
 
 
 
 @NgModule({
-	declarations: [DashboardComponent, DashboardPomodoroProgressComponent],
+	declarations: [DashboardComponent, DashboardPomodoroProgressComponent, DashboardTaskItemComponent],
 	imports: [
 		SharedModule,
 		DashboardRoutingModule
--- src/app/protected/dashboard/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard/dashboard.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {DateService} from '../../../core/services/date.service';
+import {User} from '../../../shared/models/user';
+import {Observable} from 'rxjs';
+import {Workday} from '../../../shared/models/workday';
+import {AuthService} from '../../../core/services/auth.service';
+import {WorkdaysService} from '../../../core/services/workdays.service';
 
 @Component({
 	selector: 'al-dashboard',
@@ -8,11 +13,15 @@ import {DateService} from '../../../core/services/date.service';
 })
 export class DashboardComponent implements OnInit {
 	currentDate: string;
+	currentUser: User;
+	workday$: Observable<Workday>;
 
-	constructor(private dateService: DateService) { }
+	constructor(private authService: AuthService, private dateService: DateService, private workdaysService: WorkdaysService) { }
 
 	ngOnInit() {
 		this.currentDate = this.dateService.getDisplayDate(new Date());
+		this.currentUser = this.authService.currentUser;
+		this.workday$ = this.workdaysService.getWorkdayByDate(this.currentDate, this.currentUser.id);
 	}
 
 }
