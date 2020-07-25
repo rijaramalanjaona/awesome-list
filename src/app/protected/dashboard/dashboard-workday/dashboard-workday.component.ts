d880e083906d2617d934cf09c8d5efca7a706224
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-07-25 15:30:16 +0200

    Mise en place des 4 flux du FOOC

--- src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
+++ src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Workday} from '../../../shared/models/workday';
+import {interval, Observable, Subject} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
 	selector: 'al-dashboard-workday',
@@ -12,10 +14,32 @@ export class DashboardWorkdayComponent implements OnInit {
 
 	isPomodoroActive: boolean;
 
+	// flux représentant des événements => Subject, ces flux peuvent être pilotés directement depuis le code
+	startPomodoro$: Subject<string>;
+	cancelPomodoro$: Subject<string>;
+	completePomodoro$: Subject<string>;
+
+	currentProgress: number;
+	maxProgress: number; // durée d'un pomodoro
+
+	// flux correspondant au temps qui s'écoule => Observable, émettre le nb de secondes écoulées depuis le démarrage du pomodoro
+	pomodoro$: Observable<number>;
+
 	constructor() {}
 
 	ngOnInit(): void {
 		this.isPomodoroActive = false;
+
+		this.startPomodoro$ = new Subject<string>();
+		this.cancelPomodoro$ = new Subject<string>();
+		this.completePomodoro$ = new Subject<string>();
+
+		this.currentProgress = 0;
+		this.maxProgress = 5;
+
+		this.pomodoro$ = interval(1000).pipe(
+			map(x => x + 1)
+		);
 	}
 
 	startPomodoro() {
