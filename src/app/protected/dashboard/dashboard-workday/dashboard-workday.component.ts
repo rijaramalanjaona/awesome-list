1f79b8d62196a06e007bfc79d8b3e6344a32cba2
rijaramalanjaona <rija.ramalanjaona@gmail.com>
2020-07-25 14:31:21 +0200

    dashboard : pomodoro en cours ?

--- src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
+++ src/app/protected/dashboard/dashboard-workday/dashboard-workday.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Workday} from '../../../shared/models/workday';
 
 @Component({
@@ -6,7 +6,15 @@ import {Workday} from '../../../shared/models/workday';
 	templateUrl: './dashboard-workday.component.html',
 	styleUrls: ['./dashboard-workday.component.scss']
 })
-export class DashboardWorkdayComponent {
+export class DashboardWorkdayComponent implements OnInit {
 	@Input()
 	workday: Workday;
+
+	isPomodoroActive: boolean;
+
+	constructor() {}
+
+	ngOnInit(): void {
+		this.isPomodoroActive = false;
+	}
 }
